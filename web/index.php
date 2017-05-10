aae02e3936ff6580e6a7a1c4733d36dac36a4d08
ruison1988 <ruison1988@gmail.com>
2017-05-10 18:36:36 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
 <?php
-echo "AAAAAAAAAAA";exit();
+$db = $_ENV["DATABASE_URL"];
+print_r($db);
+exit();
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
