b5b40a6670ee0b828daf608eb271de71ea904656
ruison1988 <ruison1988@gmail.com>
2017-05-11 14:51:19 +0800

    Update wp-config.php

--- web/wp-config.php
+++ web/wp-config.php
@@ -27,10 +27,10 @@ define('DB_PASSWORD', $db["pass"]);
 define('DB_HOST', $db["host"].':'.$db["port"]);
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+//define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+//define('DB_COLLATE', '');
 
 /**#@+
  * Authentication Unique Keys and Salts.
